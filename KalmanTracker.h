be3e50f068cb6dd1517f86b3f8c807c25fd35e78
Zsimeon <zuosiming1993@outlook.com>
2019-10-21 16:44:37 +0800

    Create KalmanTracker.h

---
+++ KalmanTracker.h
@@ -0,0 +1,73 @@
+#ifndef KALMAN_H
+#define KALMAN_H 2
+
+#include "opencv2/video/tracking.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+using namespace std;
+using namespace cv;
+
+#define StateType Rect<float>
+
+//此类表示作为边界框观察的各个跟踪对象的内部状态。
+class KalmanTracker
+{
+public:
+	KalmanTracker()
+	{
+		init_kf(StateType(), int());
+		m_time_since_update = 0;
+		m_hits = 0;
+		m_hit_streak = 0;
+		m_age = 0;
+		m_id = kf_count;
+		kf_count++;
+		classId;
+	}
+	KalmanTracker(StateType initRect, int)
+	{
+		init_kf(initRect, int());
+		m_time_since_update = 0;
+		m_hits = 0;
+		m_hit_streak = 0;
+		m_age = 0;
+		m_id = kf_count;
+		kf_count++;
+		classId;
+	}
+  
+  ~KalmanTracker()
+	{
+		m_history.clear();
+	}
+
+	StateType predict();
+	void update(StateType stateMat, int ClassId);
+	
+	StateType get_state();
+	StateType get_rect_xysr(float cx, float cy, float s, float r);
+
+	static int kf_count;
+
+	int m_time_since_update;
+	int m_hits;
+	int m_hit_streak;
+	int m_age;
+	int m_id;
+	int classId;
+
+private:
+	void init_kf(StateType stateMat,int ClassId);
+
+	cv::KalmanFilter kf;
+	cv::Mat measurement;
+
+	std::vector<StateType> m_history;
+};
+
+
+#endif
+
+
+
+
